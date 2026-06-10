bbf8ca8bca4492d4daf84b3f42a0eeb9946efe5e
Davi Benucci <davibenucci1042@gmail.com>
2026-06-10 14:22:40 -0300

    conclusão das funções, Arrow function e função anônima

--- JavaScript/Arrays, objetos e funções/funções.js
+++ JavaScript/Arrays, objetos e funções/funções.js
@@ -1,11 +1,13 @@
-// //Funções em JavaScript
+// TODO: Funções em JavaScript
 
-// function saudação(nome) {
-//     return `Bom dia ${nome}`
-// }
+//? Função Padrão
 
-// const variavel = saudação("Davi")
-// // console.log(variavel)
+function saudação(nome) {
+    return `Bom dia ${nome}`
+}
+
+const variavel = saudação("Davi")
+// console.log(variavel)
 
 // Ás funções são blocos de código separados do código principal aonde o código fora da função não 
 // consegue interferir no que está dentro da função. 
@@ -20,3 +22,28 @@ function somar(x, y) {
 }
 
 console.log(somar("Davi", "Israel"))
+
+
+function soma(x = 1, y = 1) {
+    return Number(x + y) // Se não for específicado o X e nem o Y eles vão continuar valendo 1, pois foi o que eu coloquei no parâmetro
+}
+
+console.log(soma(2, 2)) // agora se eu modificar o parâmetro ele vai passar a utilizar o que eu coloquei aqui.
+
+//? Função anônima 
+
+const raiz = function (n) {
+    return n ** 0.5
+}
+
+console.log(raiz(144))
+
+
+//? ARROW FUNCTION
+
+const arrow = (n) => {
+    return n ** 0.5
+}
+
+const oneLine = (n) => n ** 0.5 // Se na função houver somente uma linha, 
+// podemos remover os parênteses e o return utilizando o => para simplificarmos mais o código
